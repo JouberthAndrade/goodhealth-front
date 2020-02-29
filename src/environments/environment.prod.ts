8cb088d39af48e3a27e3ae3c9975328dc267156d
Jouberth Andrade <andrade.jouberth@gmail.com>
2020-02-28 21:32:11 -0300

    pipeline

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 export const environment = {
-  production: true
+    // tslint:disable-next-line:quotemark
+    databaseName: "dbGoodHealth",
+    databaseVersion: 1,
+    production: false,
+    versionBase: 'v1/',
+    baseUrl: 'http://localhost:5101/api/',
+    instrumentationKey: ''
 };
+
+export default environment;
